ea7306f13263fc00d648f2282355f0f3edf31f75
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-22 15:41:43 -0500

    Updates

---
+++ precache-manifest.7dc909d6ae7ee36c3029a3780792221f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f255b974d705295e6e1aa940042803",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a3d0a2332d2c3e2d09ea",
+    "url": "/clicky-game/static/css/main.3fda255f.chunk.css"
+  },
+  {
+    "revision": "f1f87615b1956b1ec567",
+    "url": "/clicky-game/static/js/2.1df37491.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.1df37491.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3d0a2332d2c3e2d09ea",
+    "url": "/clicky-game/static/js/main.e4e8c87a.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
